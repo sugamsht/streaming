b1f6bea7cab8e4a864cd358a85bb5f4811b49d91
Sugam Shrestha <sugamsht143@gmail.com>
2022-06-10 23:34:32 +0545

    Modal with Portal

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import Modal from '../Modal'
 
 export default function StreamDelete() {
     return (
-        <div>StreamDelete</div>
+        <Modal>
+            <div className="header">Delete Stream</div>
+            <div className="content">
+                Are you sure you want to delete this stream?
+            </div>
+            <div className="actions">
+                <button className="ui primary button">Delete</button>
+                <button className="ui button">Cancel</button>
+            </div>
+        </Modal>
     )
 }
