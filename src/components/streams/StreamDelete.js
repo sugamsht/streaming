bfd822594defac2d3045bc2a34fac6c78eb9b27f
Sugam Shrestha <sugamsht143@gmail.com>
2022-06-11 00:23:36 +0545

    Made Modal Reusable

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import Modal from '../Modal'
+import history from '../../history'
+
 
 export default function StreamDelete() {
+
+    const action = (
+        <div className="p-3  mt-2 text-center space-x-4 md:block">
+            <button className="mb-2 md:mb-0 bg-white px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-full hover:shadow-lg hover:bg-gray-100">
+                Cancel
+            </button>
+            <button className="mb-2 md:mb-0 bg-red-500 border border-red-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-red-600">Delete</button>
+        </div>
+    )
+
     return (
-        <Modal>
-            <div className="header">Delete Stream</div>
-            <div className="content">
-                Are you sure you want to delete this stream?
-            </div>
-            <div className="actions">
-                <button className="ui primary button">Delete</button>
-                <button className="ui button">Cancel</button>
-            </div>
-        </Modal>
+        <Modal
+            title="Are you sure?"
+            content="Do you really want to delete your Stream? This process cannot be undone"
+            action={action}
+            onDismiss={() => history.push('/')}
+        />
     )
 }
