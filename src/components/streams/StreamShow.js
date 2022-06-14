513f85ccec318699d9d66e31e8d0d8f4929735d1
Sugam Shrestha <sugamsht143@gmail.com>
2022-06-14 23:58:05 +0545

    flv.js node package

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -2,8 +2,12 @@ import React from 'react'
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
+import flv from 'flv.js';
 
 function StreamShow(props) {
+
+    const videoRef = React.createRef();
+
 
     useEffect(() => {
         props.fetchStream(props.match.params.id);
@@ -14,6 +18,7 @@ function StreamShow(props) {
     return (
         (props.stream) ? (
             <div className="flex flex-col max-w-2xl mx-[10vw] my-5">
+                <video ref={videoRef} style={{ width: '100%' }} controls />
                 <h1 className='text-2xl'> {props.stream.title} </h1>
                 <p> {props.stream.description} </p>
             </div>
